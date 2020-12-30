7a072069d33b848f03e99330618280b93892541e
poorva-prosolstech <poorva.k@prosolstech.com>
2020-12-30 17:27:52 +0530

    third commit

---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,4 @@
+/// Define App ID and Token
+const APP_ID = "b0d7fcc30bc6447196fd5047f0e8aede";
+const Token =
+    "0064cf9d67971934347b20c044f2540373dIAAYCT3+xrIYw67OxvjqMyGfl73LkjHUJHGkyZs38UkTE/2gtqAAAAAAEACyc3BiGJ3tXwEAAQAUne1f";
